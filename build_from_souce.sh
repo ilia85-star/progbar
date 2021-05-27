a838630744ba9c6a97bcbcc884ad575c3085549a
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2021-05-27 19:57:56 +0430

    Stable version

---
+++ build_from_souce.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+cd ~
+git clone https://github.com/ilia85-star/pgb.git
+mkdir ~/pgb-tmp
+python3 -m pip install --upgrade pip venv
+python3 -m venv ~/pgb-tmp
+source ~/pgb-tmp/bin/activate
+python3 -m pip install --upgrade pip build
+cd ~/pgb
+python3 -m build
+deactivate
+python3 -m pip install dist/pgb-0.0.1-py3-none-any.whl
+
+rm -r ~/pgb-tmp
