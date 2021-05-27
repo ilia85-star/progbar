8d83b053b16503e684fd1e848343bd74acd6f1d2
Ilia Mahjour-Shafiei <ilia.mahjour.shafiei@outlook.com>
2021-05-27 20:13:47 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setuptools.setup(
         "Bug Tracker": "https://github.com/ilia85-star/pgb/issues",
     },
     entry_points={
-        "console_scripts": ["pgb = pgb.pgb"],
+        "console_scripts": ["pgb = pgb.__main__:main"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
